1d14d520eb16fe2fd660d9031f08ebc35fc2c311
Amar Tiric <amar.tiric@globallogic.com>
2018-08-20 15:34:15 +0200

    add scheduled task for loading vacation and sprint data

--- core/src/main/java/com/globallogic/dashboard/vacation/VacationResource.java
+++ core/src/main/java/com/globallogic/dashboard/vacation/VacationResource.java
@@ -1,5 +1,7 @@
 package com.globallogic.dashboard.vacation;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
