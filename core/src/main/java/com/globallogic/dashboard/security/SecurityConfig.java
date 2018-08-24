a2cc47138f3458d81602135ec70343401071e758
Amar Tiric <amar.tiric@globallogic.com>
2018-08-24 16:27:16 +0200

    remove commented unused code

--- core/src/main/java/com/globallogic/dashboard/security/SecurityConfig.java
+++ core/src/main/java/com/globallogic/dashboard/security/SecurityConfig.java
@@ -89,20 +89,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .csrf().disable();
     }
 
-    /*
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }*/
-
-
     @Bean
     public CorsFilter corsFilter() {
 
