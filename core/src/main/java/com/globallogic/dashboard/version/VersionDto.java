a53a7b621de6cee10b763469dee0468dd279786b
TimkoMilan <milan.timko@akademiasovy.sk>
2018-10-15 08:13:30 +0200

    Api for server version

---
+++ core/src/main/java/com/globallogic/dashboard/version/VersionDto.java
@@ -0,0 +1,22 @@
+package com.globallogic.dashboard.version;
+
+public class VersionDto {
+    private String version;
+    private String releaseDate;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
---
+++ core/src/test/java/com/globallogic/dashboard/user/UserResourceTest.java
@@ -0,0 +1,12 @@
+package com.globallogic.dashboard.user;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserResourceTest {
+
+    @Test
+    public void login() {
+    }
+}
