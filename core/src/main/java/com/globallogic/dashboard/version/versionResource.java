bc4da07228bef5347f6d462e7a5938c4826c12cb
TimkoMilan <milan.timko@akademiasovy.sk>
2018-10-17 13:19:38 +0200

    TemaName in token

--- core/src/main/java/com/globallogic/dashboard/version/versionResource.java
+++ core/src/main/java/com/globallogic/dashboard/version/versionResource.java
@@ -1,18 +1,25 @@
 package com.globallogic.dashboard.version;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@PropertySource("classpath:/application.properties")
 @RestController
 @RequestMapping("version")
 public class versionResource {
+    @Value("${server.version}") private String version;
+    @Value("${server.version.releaseDate}")private String releaseDate;
 
 @GetMapping
 public VersionDto version(){
-    VersionDto versionDto = new VersionDto();
-    versionDto.setReleaseDate("3.10.2018");
-    versionDto.setVersion("1.10");
-    return versionDto;
+
+    VersionDto dto = new VersionDto();
+    dto.setVersion(version);
+    dto.setReleaseDate(releaseDate);
+    return dto;
+
 }
 }
