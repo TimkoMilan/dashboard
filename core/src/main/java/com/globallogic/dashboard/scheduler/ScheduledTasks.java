4e4ceed71d7fd74cc1a23ac83985254114701f45
TimkoMilan <milan.timko@akademiasovy.sk>
2019-01-09 13:11:58 +0100

    Add function to send email to user , validation token

--- core/src/main/java/com/globallogic/dashboard/scheduler/ScheduledTasks.java
+++ core/src/main/java/com/globallogic/dashboard/scheduler/ScheduledTasks.java
@@ -2,6 +2,7 @@ package com.globallogic.dashboard.scheduler;
 
 import com.globallogic.dashboard.sprint.SprintDataFacade;
 import com.globallogic.dashboard.vacation.VacationFacade;
+import com.globallogic.dashboard.validationToken.TokenService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +11,12 @@ public class ScheduledTasks {
 
     private final VacationFacade vacationFacade;
     private final SprintDataFacade sprintDataFacade;
+    private final TokenService tokenService;
 
-    public ScheduledTasks(final VacationFacade vacationFacade, final SprintDataFacade sprintDataFacade) {
+    public ScheduledTasks(final VacationFacade vacationFacade, final SprintDataFacade sprintDataFacade, TokenService tokenService) {
         this.vacationFacade = vacationFacade;
         this.sprintDataFacade = sprintDataFacade;
+        this.tokenService = tokenService;
     }
 
     @Scheduled(cron = "${cron.dataLoader.expression}")
@@ -21,5 +24,10 @@ public class ScheduledTasks {
         vacationFacade.loadVacation();
         sprintDataFacade.loadSprintData();
     }
+    @Scheduled(cron ="${cron.dataLoader.expression}")
+    public void checkTokenValidation(){
+        tokenService.checkTokenValidation();
+    }
+
 
 }
